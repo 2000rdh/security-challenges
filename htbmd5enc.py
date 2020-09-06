2fda08a6e3132daa0bc3ea114fe64325bf84a723
2000rdh <43261008+2000rdh@users.noreply.github.com>
2020-09-06 18:16:02 -0400

    Update htbmd5enc.py

--- htbmd5enc.py
+++ htbmd5enc.py
@@ -1,15 +1,27 @@
+#This Python program was written for "emdee five for life" on hackthebox.eu
+#It pulls a string from the challenge's webpage and hashes it, then posts it back
+
 import requests
 import hashlib
 from bs4 import BeautifulSoup
 
-url = 'http://docker.hackthebox.eu:31516'
+url = 'http://docker.hackthebox.eu:[port]'
 
+#create a session
 s = requests.session()
+
+#extract the necessary string from the HTML
 md5string = s.get(url)
 temp = BeautifulSoup(md5string.content, 'html5lib')
 temp2 = temp.find('h3').getText()
+
+#hash the string
 result = (hashlib.md5(temp2.encode('utf-8'))).hexdigest()
+
+#post the hash
 data = {'hash': result}
 resp = s.post(url, data = data)
+
+#get the result!
 t = BeautifulSoup(resp.content, 'html5lib')
 print(t)
